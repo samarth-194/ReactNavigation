93781efdf5e6a1a488aa69a22c5d949d264c6c9e
Thosar Samarth INPU <Samarth.Thosar@syngenta.com>
2025-02-07 12:04:29 +0530

    Icons working

--- Screens/Notifications.tsx
+++ Screens/Notifications.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
+import EvilIcons from "react-native-vector-icons/EvilIcons";
+import FontAwesome6Icon from "react-native-vector-icons/FontAwesome6";
+
 
 const Notifications=()=>{
     return(
         <SafeAreaView>
+            <FontAwesome6Icon name="comments" color="#000000"size={100} />;
+            <EvilIcons
+            name="clock"color="#000000" size={100}
+            />
             <Text>This is the NewScreen.</Text>
         </SafeAreaView>
     );
